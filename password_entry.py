c001eee62b4811879a1af41fa1b3df936cc93731
Andrew Gibson <Andrew.gibson@my.jcu.edu.au>
2019-03-27 22:01:13 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,13 @@
 """Andrew Gibson"""
+
+
+
+
+MINIMUM_LENGTH = 6
+
+"""Get password of valid size from user and print asterisks"""
+password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+while len(password) < MINIMUM_LENGTH:
+    password = input("Enter password of at least {} characters: ".format(MINIMUM_LENGTH))
+
+print('*' * len(password))
